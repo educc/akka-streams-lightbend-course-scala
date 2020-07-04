9c6de588ad6d3d5c96e0c4460edf92ac64e30496
ecacho <ecachocasas@gmail.com>
2020-07-04 12:11:01 -0500

    building streams

--- exercises/exercises/src/main/scala/com/lightbend/akkassembly/QualityAssurance.scala
+++ exercises/exercises/src/main/scala/com/lightbend/akkassembly/QualityAssurance.scala
@@ -1,14 +1,35 @@
 package com.lightbend.akkassembly
 
 import akka.NotUsed
+import akka.stream.{ActorAttributes, Supervision}
 import akka.stream.scaladsl.Flow
+import com.lightbend.akkassembly.QualityAssurance.CardFailedInspection
+
+object QualityAssurance {
+
+  class CardFailedInspection(car: UnfinishedCar) extends IllegalStateException("Car doesn't pass inspection")
+
+}
 
 class QualityAssurance {
 
-  val inspect : Flow[UnfinishedCar, Car, NotUsed] = Flow[UnfinishedCar]
-    .collect {
-      case x if x.wheels.size == 4 && x.engine.isDefined && x.color.isDefined =>
-        Car(SerialNumber(), x.color.get, x.engine.get, wheels = x.wheels, upgrade = Option.empty)
+  val decider: Supervision.Decider = {
+    case _: CardFailedInspection => Supervision.resume
+  }
+
+  val inspect: Flow[UnfinishedCar, Car, NotUsed] = Flow[UnfinishedCar]
+    .map { x =>
+      if (x.wheels.size != 4 ||
+        x.engine.isEmpty ||
+        x.color.isEmpty) {
+        throw new CardFailedInspection(x)
+      }
+
+      Car(SerialNumber(), x.color.get, x.engine.get, wheels = x.wheels, upgrade = Option.empty)
+    }
+    .collect { case x => x }
+    .withAttributes {
+      ActorAttributes.supervisionStrategy(decider)
     }
 
 }
