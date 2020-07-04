a31307f6f50f1bce2c5952595e8cd485936d250d
ecacho <ecachocasas@gmail.com>
2020-07-04 11:53:13 -0500

    run a graph

--- exercises/exercises/src/main/scala/com/lightbend/akkassembly/Auditor.scala
+++ exercises/exercises/src/main/scala/com/lightbend/akkassembly/Auditor.scala
@@ -2,12 +2,13 @@ package com.lightbend.akkassembly
 
 import akka.{Done, NotUsed}
 import akka.event.LoggingAdapter
-import akka.stream.scaladsl.{Flow, Sink}
+import akka.stream.Materializer
+import akka.stream.scaladsl.{Flow, Sink, Source}
 
 import scala.concurrent.Future
 import scala.concurrent.duration.FiniteDuration
 
-class Auditor {
+class Auditor(implicit mat: Materializer) {
 
   val count: Sink[Any, Future[Int]] = Sink.fold(0) {
     case (sum , elem) => sum+1
@@ -18,4 +19,12 @@ class Auditor {
   }
 
   def sample(simpleSize: FiniteDuration): Flow[Car, Car, NotUsed] = Flow[Car].takeWithin(simpleSize)
+
+  def audit(source: Source[Car,NotUsed], sampleSize: FiniteDuration): Future[Int] = {
+    source
+      .via(sample((sampleSize)))
+      .runFold(0) {
+        case (sum, _) => sum +1
+      }
+  }
 }
---
+++ exercises/exercises/src/main/scala/com/lightbend/akkassembly/Factory.scala
@@ -0,0 +1,25 @@
+package com.lightbend.akkassembly
+
+import akka.stream.Materializer
+import akka.stream.scaladsl.Sink
+
+import scala.concurrent.Future
+
+class Factory(
+               bodyShop: BodyShop,
+               paintShop: PaintShop,
+               engineShop: EngineShop,
+               wheelShop: WheelShop,
+               qualityAssurance: QualityAssurance,
+               )(implicit val mat: Materializer) {
+
+  def orderCars(quantity: Int): Future[Seq[Car]] = {
+    bodyShop.cars.take(quantity)
+      .via(paintShop.paint)
+      .via(engineShop.installEngine)
+      .via(wheelShop.installWheels)
+      .via(qualityAssurance.inspect)
+      .runWith(Sink.seq)
+  }
+
+}
